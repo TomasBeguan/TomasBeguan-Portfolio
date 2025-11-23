f2f5c3eb71ade4b3b5d6011a9f9d5a7365ce96ad
TomasBeguan <tomasbeguan@gmail.com>
2025-11-23 16:46:16 -0300

    Fix RetroButton type error for build

--- src/components/RetroButton.tsx
+++ src/components/RetroButton.tsx
@@ -2,29 +2,21 @@
 
 import { cn } from "@/lib/utils";
 
-interface RetroButtonProps {
+interface RetroButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
-    onClick?: () => void;
     className?: string;
-    style?: React.CSSProperties;
+    onClick?: () => void;
 }
 
-export const RetroButton = ({ children, onClick, className = "", style }: RetroButtonProps) => {
+export const RetroButton = ({ children, className, onClick, ...props }: RetroButtonProps) => {
     return (
         <button
             onClick={onClick}
-            style={style}
-            className={`
-        px-8 py-2
-        bg-white dark:bg-retro-dark-blue border-2 border-black dark:border-white
-        font-silkscreen text-lg text-black dark:text-white
-        shadow-retro dark:shadow-[4px_4px_0px_0px_#ffffff] 
-        hover:shadow-retro-sm dark:hover:shadow-[2px_2px_0px_0px_#ffffff] 
-        hover:translate-x-[2px] hover:translate-y-[2px]
-        hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black
-        transition-all active:shadow-none active:translate-x-[4px] active:translate-y-[4px]
-        ${className}
-      `}
+            className={cn(
+                "border-2 border-black dark:border-white shadow-retro dark:shadow-[4px_4px_0px_0px_#ffffff] active:translate-x-[2px] active:translate-y-[2px] active:shadow-none transition-all bg-white dark:bg-retro-dark-blue text-black dark:text-white font-chicago px-4 py-1 hover:bg-gray-100 dark:hover:bg-gray-800",
+                className
+            )}
+            {...props}
         >
             {children}
         </button>
